8d5160fe08f354dc6d2ce369a4e185614d913fa1
Johnathon Selstad <makeshifted@gmail.com>
2017-02-12 23:49:12 -0800

    Add Illusion Demonstration Scene

---
+++ Assets/Projection/Illusion/IllusionFollower.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class IllusionFollower : MonoBehaviour {
+  public Camera mainCamera;
+  public AnimationCurve CameraFallOff;
+  Camera illusionCam;
+  void Start() {
+    illusionCam = GetComponent<Camera>();
+    illusionCam.fieldOfView = mainCamera.fieldOfView + 60f;
+  }
+	void LateUpdate () {
+    transform.position = Vector3.Lerp(transform.position, mainCamera.transform.position, CameraFallOff.Evaluate(-mainCamera.transform.position.z));
+    transform.rotation = Quaternion.Slerp(transform.rotation, mainCamera.transform.rotation, CameraFallOff.Evaluate(-mainCamera.transform.position.z));
+  }
+}
---
+++ Assets/Projection/Illusion/MinimalFPSController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+[RequireComponent(typeof(CharacterController))]
+public class MinimalFPSController : MonoBehaviour {
+  CharacterController controller;
+  void Start() {
+    controller = GetComponent<CharacterController>();
+  }
+  void Update() {
+    Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+    transform.Rotate(0, Input.GetAxis("Mouse X") * 3f, 0);
+    Camera.main.transform.localRotation *= Quaternion.Euler(-Input.GetAxis("Mouse Y") * 3f, 0, 0);
+    controller.Move(transform.rotation * movement * Time.deltaTime);
+  }
+}
