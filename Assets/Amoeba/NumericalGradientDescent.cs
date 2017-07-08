cc6738b78f794549d505211111c1870e85a05903
Johnathon Selstad <makeshifted@gmail.com>
2017-07-08 15:06:53 -0700

    Add Numerical Gradient Descent for comparison
    I generally don't like it as much

---
+++ Assets/Amoeba/NumericalGradientDescent.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+
+//Numerical Gradient Descent Implementation-----------------------------------------------------------
+public class NumericalGradientDescent {
+  public List<NelderMead.Vertex> simplexVertices;
+  public float alpha, epsilon;
+  Func<float[], float> costFunc;
+
+  //Initializes the solver with a starting coordinate, a simplex dimension, 
+  //and coefficients (strengths) for the various transformations.
+  public NumericalGradientDescent(
+    float[] initialVertex,
+    Func<float[], float> costFunction,
+    float stepSize = 1f,
+    float differentiationEpsilon = 0.0001f) {
+
+    //Assign the cost function; this function takes in a coordinate in parameter space
+    //And outputs the "cost" associated with this parameter; the solver will try to minimize the cost
+    costFunc = costFunction;
+
+    //These hyperparameters control how the solver converges.
+    //Increasing the alpha will make it more aggressive in how it converges
+    alpha = stepSize;
+    //Increasing the epsilon will make the gradient consider a wider region of parameter space.
+    epsilon = differentiationEpsilon;
+
+    //Create Initial Simplex: Make the first vertex the initialVertex
+    //And all subsequent vertices are translated "differentiationEpsilon"
+    //along just that dimension.  This is a "Right Angle" Simplex
+    simplexVertices = new List<NelderMead.Vertex>(initialVertex.Length + 1);
+    constructRightAngleSimplex(initialVertex, differentiationEpsilon);
+  }
+
+  //Calculate the gradient according to epsilon and step along it according to alpha.
+  public void stepSolver() {
+    for(int i = 0; i < simplexVertices[0].coordinates.Length; i++) {
+      simplexVertices[0].coordinates[i] += (-(simplexVertices[i + 1].cost - simplexVertices[0].cost) / epsilon) * alpha;
+    }
+
+    updateRightAngleSimplex(epsilon);
+  }
+
+  //This creates a right-angle simplex around the "initial vertex" position
+  public void constructRightAngleSimplex(float[] initialVertex, float simplexSize) {
+    simplexVertices.Clear();
+    simplexVertices.Add(new NelderMead.Vertex(initialVertex, costFunc, 0));
+    for (int i = 0; i < initialVertex.Length; i++) {
+      float[] vertexCoordinate = new float[initialVertex.Length];
+      Array.Copy(initialVertex, vertexCoordinate, initialVertex.Length);
+      vertexCoordinate[i] += simplexSize;
+
+      simplexVertices.Add(new NelderMead.Vertex(vertexCoordinate, costFunc, i + 1));
+    }
+  }
+
+  //This reconstructs the right-angle simplex around the first vertex
+  public void updateRightAngleSimplex(float simplexSize) {
+    simplexVertices[0] = new NelderMead.Vertex(simplexVertices[0].coordinates, costFunc, 0);
+    for (int i = 0; i < simplexVertices[0].coordinates.Length; i++) {
+      Array.Copy(simplexVertices[0].coordinates, simplexVertices[i + 1].coordinates, simplexVertices[0].coordinates.Length);
+      simplexVertices[i + 1].coordinates[i] += simplexSize;
+
+      simplexVertices[i + 1] = new NelderMead.Vertex(simplexVertices[i + 1].coordinates, costFunc, i + 1);
+    }
+  }
+}
---
+++ Assets/Amoeba/NumericalGradientDescentIK.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class NumericalGradientDescentIK : MonoBehaviour {
+  public Transform Tooltip;
+  public Transform Target;
+  public Transform[] joints;
+  public float stepSize = 50f;
+  public float epsilon = 0.001f;
+  public int iterationsPerFrame = 5;
+
+  NumericalGradientDescent solver;
+
+  //Initialize the solver with a neutral starting state and a cost function
+  void Start() {
+    float[] initialCoord = new float[joints.Length];
+    solver = new NumericalGradientDescent(initialCoord, distanceCost, stepSize, epsilon);
+  }
+
+  void Update() {
+    //Allow for the modification of the solver parameters after initialization
+    solver.alpha = stepSize;
+    solver.epsilon = epsilon;
+
+    //Step the solver forward
+    for (int i = 0; i < iterationsPerFrame; i++) {
+      solver.stepSolver();
+    }
+  }
+
+  //This evaluates the cost associated with a coordinate in parameter space
+  //This particular function simply represents the distance that the tooltip is away from the target.
+  public float distanceCost(float[] coordinate) {
+    joints[0].localRotation = Quaternion.Euler(0f, coordinate[0], 0f);
+    joints[1].localRotation = Quaternion.Euler(coordinate[1], 0f, 0f);
+    joints[2].localRotation = Quaternion.Euler(coordinate[2], 0f, 0f);
+    joints[3].localRotation = Quaternion.Euler(coordinate[3], 0f, 0f);
+    joints[4].localRotation = Quaternion.Euler(0f, coordinate[4], 0f);
+
+    return (Tooltip.position - Target.position).magnitude;
+  }
+}
