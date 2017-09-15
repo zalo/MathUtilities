e68aec63ad49421d851c38cfecfe4f19b67cfd86
Johnathon Selstad <makeshifted@gmail.com>
2017-09-15 00:04:15 -0700

    Fix stability and add linear alpha estimate
    Seems to help convergence times a ton; why didn't I try this earlier?
    
    Might help the minkowski sum raymarcher too...

--- Assets/BundleAdustment/SpatialBundleAdjuster.cs
+++ Assets/BundleAdustment/SpatialBundleAdjuster.cs
@@ -3,10 +3,12 @@
 public class SpatialBundleAdjuster : MonoBehaviour {
   public Transform[] features;
   public Transform aligningCamera;
+  public bool useLinearEstimate = true;
   Vector3[] rayDirections;
 
   KabschSolver solver = new KabschSolver();
   Vector3[] inPoints, refPoints;
+  float previousCost = 1000f;
   void Start() {
     rayDirections = new Vector3[features.Length];
     inPoints = new Vector3[features.Length];
@@ -19,17 +21,31 @@ public class SpatialBundleAdjuster : MonoBehaviour {
   }
 
   void Update() {
-    for (int iteration = 0; iteration < 1; iteration++) {
+    solver = new KabschSolver();
+
+    for (int iteration = 0; iteration < 2; iteration++) {
+      float currentCost = 0f;
+
       for (int i = 0; i < features.Length; i++) {
         refPoints[i] = features[i].position;
-        inPoints[i] = Constraints.ConstrainToSegment(features[i].position, aligningCamera.position, aligningCamera.TransformPoint(rayDirections[i] * 6f));
-        Debug.DrawLine(aligningCamera.position, aligningCamera.TransformPoint(rayDirections[i] * 6f));
+        inPoints[i] = Constraints.ConstrainToSegment(features[i].position, aligningCamera.position, aligningCamera.TransformPoint(rayDirections[i]*6f));
+        currentCost += Vector3.Distance(refPoints[i], inPoints[i]);
+        Debug.DrawLine(aligningCamera.position, aligningCamera.TransformPoint(rayDirections[i]));
         Debug.DrawLine(features[i].position, inPoints[i], Color.red);
       }
 
       Matrix4x4 iterationStep = solver.SolveKabsch(inPoints, refPoints, true);
-      aligningCamera.position += iterationStep.GetVector3();
-      aligningCamera.rotation = iterationStep.GetQuaternion() * aligningCamera.rotation;
+      Matrix4x4 steppedTransform = iterationStep * aligningCamera.localToWorldMatrix;
+      if (useLinearEstimate) {
+        float stepScale = ((previousCost - currentCost) > 0.0000001f && iteration > 0) ? (previousCost / (previousCost - currentCost)) * 0.8f : 1f;
+        aligningCamera.position += (steppedTransform.GetVector3() - aligningCamera.position) * stepScale;
+        aligningCamera.rotation = Quaternion.SlerpUnclamped(Quaternion.identity, iterationStep.GetQuaternion(), stepScale) * aligningCamera.rotation;
+      } else {
+        aligningCamera.position = steppedTransform.GetVector3();
+        aligningCamera.rotation = steppedTransform.GetQuaternion();
+      }
+
+      previousCost = currentCost;
     }
   }
 }
