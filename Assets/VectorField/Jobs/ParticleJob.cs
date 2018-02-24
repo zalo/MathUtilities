809a6670031320777d252b9dcd084081e6f628a6
Johnathon Selstad <makeshifted@gmail.com>
2018-02-24 14:39:34 -0800

    Use GPU Instancing to Draw Particle System

--- Assets/VectorField/Jobs/ParticleJob.cs
+++ Assets/VectorField/Jobs/ParticleJob.cs
@@ -4,19 +4,24 @@ using Unity.Collections;
 
 public class ParticleJob : MonoBehaviour {
   public VectorField vectorField;
-  public int numParticles = 500;
+  public Mesh sphereMesh;
+  public Material sphereMaterial;
+  int numParticles = 1023;
   ParticleData particleData;
-  Vector3[] particles;
+  Matrix4x4[] particles;
 
   public struct ParticleData : IJobParallelFor, System.IDisposable {
     [ReadOnly]
     public VectorFieldStruct field;
     public NativeArray<Vector3> particlesCurrent;
     public NativeArray<Vector3> particlesPrevious;
+    [WriteOnly]
+    public NativeArray<Matrix4x4> particleMatrices;
 
     public ParticleData(int numParticles, VectorFieldStruct field) {
       particlesCurrent = new NativeArray<Vector3>(numParticles, Allocator.Persistent);
       particlesPrevious = new NativeArray<Vector3>(numParticles, Allocator.Persistent);
+      particleMatrices = new NativeArray<Matrix4x4>(numParticles, Allocator.Persistent);
       for (int i = 0; i < numParticles; i++) {
         particlesCurrent[i] = Random.insideUnitSphere + (Vector3.one*0.5f);
         particlesPrevious[i] = particlesCurrent[i] + (Random.insideUnitSphere * 0.01f);
@@ -25,37 +30,35 @@ public class ParticleJob : MonoBehaviour {
     }
 
     public void Execute(int i) {
+      //Sample the Vector Field
       particlesCurrent[i] += field.trilinearSample(particlesCurrent[i]) * 0.1f;
 
+      //Integrate the Particle via Verlet Integration
       Vector3 temp = particlesCurrent[i];
       particlesCurrent[i] += (particlesCurrent[i] - particlesPrevious[i]);
       particlesPrevious[i] = temp;
+
+      //Write the particle positions out to transformation matrices
+      particleMatrices[i] = Matrix4x4.TRS(particlesCurrent[i], Quaternion.identity, Vector3.one * 0.01f);
     }
 
     public void Dispose() {
       particlesCurrent.Dispose();
       particlesPrevious.Dispose();
+      particleMatrices.Dispose();
     }
   }
 
   void Start() {
     if (!vectorField.hasStarted) { vectorField.Start(); }
-    particles = new Vector3[numParticles];
+    particles = new Matrix4x4[numParticles];
     particleData = new ParticleData(numParticles, vectorField.field);
   }
 
   void Update() {
-    particleData.Schedule(particles.Length, 256).Complete();
-  }
-
-  public void OnDrawGizmos() {
-    if (Application.isPlaying && particles != null) {
-      particleData.particlesCurrent.CopyTo(particles);
-
-      for (int i = 0; i < particles.Length; i++) {
-        Gizmos.DrawSphere(particles[i], 0.01f);
-      }
-    }
+    particleData.Schedule(particles.Length, 128).Complete();
+    particleData.particleMatrices.CopyTo(particles);
+    Graphics.DrawMeshInstanced(sphereMesh, 0, sphereMaterial, particles);
   }
 
   void OnDestroy() {
