1dc7b4d8bc1ff1b8e77a1c1f55fef9093ef6ef56
Johnathon Selstad <makeshifted@gmail.com>
2018-02-24 15:24:45 -0800

    Refactor instancing to draw any number of particles

--- Assets/VectorField/Jobs/ParticleJob.cs
+++ Assets/VectorField/Jobs/ParticleJob.cs
@@ -3,12 +3,13 @@ using Unity.Jobs;
 using Unity.Collections;
 
 public class ParticleJob : MonoBehaviour {
+  public int numParticles = 10230;
   public VectorField vectorField;
   public Mesh sphereMesh;
   public Material sphereMaterial;
-  int numParticles = 1023;
   ParticleData particleData;
   Matrix4x4[] particles;
+  Matrix4x4[] instanceMatrices = new Matrix4x4[1023];
 
   public struct ParticleData : IJobParallelFor, System.IDisposable {
     [ReadOnly]
@@ -22,6 +23,7 @@ public class ParticleJob : MonoBehaviour {
       particlesCurrent = new NativeArray<Vector3>(numParticles, Allocator.Persistent);
       particlesPrevious = new NativeArray<Vector3>(numParticles, Allocator.Persistent);
       particleMatrices = new NativeArray<Matrix4x4>(numParticles, Allocator.Persistent);
+
       for (int i = 0; i < numParticles; i++) {
         particlesCurrent[i] = Random.insideUnitSphere + (Vector3.one*0.5f);
         particlesPrevious[i] = particlesCurrent[i] + (Random.insideUnitSphere * 0.01f);
@@ -51,14 +53,25 @@ public class ParticleJob : MonoBehaviour {
 
   void Start() {
     if (!vectorField.hasStarted) { vectorField.Start(); }
-    particles = new Matrix4x4[numParticles];
     particleData = new ParticleData(numParticles, vectorField.field);
+    particles = new Matrix4x4[numParticles];
   }
 
   void Update() {
     particleData.Schedule(particles.Length, 128).Complete();
     particleData.particleMatrices.CopyTo(particles);
-    Graphics.DrawMeshInstanced(sphereMesh, 0, sphereMaterial, particles);
+    
+    //Draw the particles 1023 at a time (since DrawMeshInstanced can't do any more at once)
+    int particlesDrawn = 0;
+    while (particlesDrawn < numParticles) {
+      int particlesToDraw = Mathf.Min(1023, numParticles - particlesDrawn);
+      //SLICES SEEM TO BE BROKEN; use slow system copy instead
+      //NativeSlice<Matrix4x4> slice = new NativeSlice<Matrix4x4>(particleData.particleMatrices, particlesDrawn, particlesToDraw);
+      //slice.CopyTo(instanceMatrices); //or //instanceMatrices = slice.toArray();
+      System.Array.Copy(particles, particlesDrawn, instanceMatrices, 0, particlesToDraw);
+      Graphics.DrawMeshInstanced(sphereMesh, 0, sphereMaterial, instanceMatrices);
+      particlesDrawn += particlesToDraw;
+    }
   }
 
   void OnDestroy() {
