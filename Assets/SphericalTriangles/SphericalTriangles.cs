eabdb2724fd34f418e96330bac898b59df167cbb
Johnathon Selstad <makeshifted@gmail.com>
2018-03-09 22:25:53 -0800

    Optimize the spherical triangle calculation a little
    Not great but not terrible either, maybe a good candidate for multithreaded voxellization...
    
    I wonder if there's a good way to go from solid angle of a mesh to distance field of a mesh... probably not...

--- Assets/SphericalTriangles/SphericalTriangles.cs
+++ Assets/SphericalTriangles/SphericalTriangles.cs
@@ -4,28 +4,26 @@ public class SphericalTriangles : MonoBehaviour {
   public Transform[] vertices = new Transform[3];
   void OnDrawGizmos() {
     if (vertices.Length == 3) {
-      Vector3[] triangleVertices = { vertices[0].position, vertices[1].position, vertices[2].position };
-      float interiorAngle = transform.position.SphericalTriangleInteriorAngleSum(triangleVertices, true);
-      Debug.Log("Sum of Interior Angles: " + transform.position.SphericalTriangleInteriorAngleSum(triangleVertices, true) + 
-        ", Area of Spherical Triangle/Solid Angle: " + transform.position.SolidAngleOfTriangle(triangleVertices));
+      Debug.Log("Sum of Interior Angles: " + transform.position.SphericalTriangleInteriorAngleSum(vertices[0].position, vertices[1].position, vertices[2].position, true) + 
+        ", Area of Spherical Triangle/Solid Angle: " + transform.position.SolidAngleOfTriangle(vertices[0].position, vertices[1].position, vertices[2].position));
     }
   }
 }
 
 public static class SphericalTriangleExtension {
-  public static float SphericalTriangleInteriorAngleSum(this Vector3 point, Vector3[] vertices, bool drawGizmos = false) {
+  public static float SphericalTriangleInteriorAngleSum(this Vector3 point, Vector3 v1, Vector3 v2, Vector3 v3, bool drawGizmos = false) {
     //Step 1: Project the Triangle Vertices onto a Unit Sphere around the point
-    for (int i = 0; i < 3; i++) {
-      vertices[i] = vertices[i].ConstrainDistance(point, 1f);
-    }
+    v1 = (v1 - point).normalized;
+    v2 = (v2 - point).normalized;
+    v3 = (v3 - point).normalized;
 
     //Step 2: Calculate the spherical tangents between the vertices of the triangle
-    Vector3 tangent01 = Vector3.ProjectOnPlane(vertices[1] - vertices[0], vertices[0] - point),
-            tangent02 = Vector3.ProjectOnPlane(vertices[2] - vertices[0], vertices[0] - point),
-            tangent10 = Vector3.ProjectOnPlane(vertices[0] - vertices[1], vertices[1] - point),
-            tangent12 = Vector3.ProjectOnPlane(vertices[2] - vertices[1], vertices[1] - point),
-            tangent21 = Vector3.ProjectOnPlane(vertices[1] - vertices[2], vertices[2] - point),
-            tangent20 = Vector3.ProjectOnPlane(vertices[0] - vertices[2], vertices[2] - point);
+    Vector3 tangent01 = Vector3.ProjectOnPlane(v2 - v1, v1),
+            tangent02 = Vector3.ProjectOnPlane(v3 - v1, v1),
+            tangent10 = Vector3.ProjectOnPlane(v1 - v2, v2),
+            tangent12 = Vector3.ProjectOnPlane(v3 - v2, v2),
+            tangent21 = Vector3.ProjectOnPlane(v2 - v3, v3),
+            tangent20 = Vector3.ProjectOnPlane(v1 - v3, v3);
 
     //Step 3: Sum the angles between the tangents
     float interiorAngle  = Vector3.Angle(tangent01, tangent02);
@@ -35,29 +33,29 @@ public static class SphericalTriangleExtension {
     if (drawGizmos) {
       for (float i = 0; i < 1.0; i += 0.01f) {
         Gizmos.DrawLine(
-          Vector3.Lerp(vertices[0], vertices[1], i).ConstrainDistance(point, 1f),
-          Vector3.Lerp(vertices[0], vertices[1], i + 0.01f).ConstrainDistance(point, 1f));
-        Gizmos.DrawLine(
-          Vector3.Lerp(vertices[1], vertices[2], i).ConstrainDistance(point, 1f),
-          Vector3.Lerp(vertices[1], vertices[2], i + 0.01f).ConstrainDistance(point, 1f));
-        Gizmos.DrawLine(
-          Vector3.Lerp(vertices[2], vertices[0], i).ConstrainDistance(point, 1f),
-          Vector3.Lerp(vertices[2], vertices[0], i + 0.01f).ConstrainDistance(point, 1f));
+          Vector3.Lerp(v1, v2, i).normalized + point,
+          Vector3.Lerp(v1, v2, i + 0.01f).normalized + point);
+        Gizmos.DrawLine(          
+          Vector3.Lerp(v2, v3, i).normalized + point,
+          Vector3.Lerp(v2, v3, i + 0.01f).normalized + point);
+        Gizmos.DrawLine(          
+          Vector3.Lerp(v3, v1, i).normalized + point,
+          Vector3.Lerp(v3, v1, i + 0.01f).normalized + point);
       }
       Gizmos.color = Color.red;
-      Gizmos.DrawRay(vertices[0], tangent01.normalized * 0.4f);
-      Gizmos.DrawRay(vertices[0], tangent02.normalized * 0.4f);
-      Gizmos.DrawRay(vertices[1], tangent10.normalized * 0.4f);
-      Gizmos.DrawRay(vertices[1], tangent12.normalized * 0.4f);
-      Gizmos.DrawRay(vertices[2], tangent21.normalized * 0.4f);
-      Gizmos.DrawRay(vertices[2], tangent20.normalized * 0.4f);
+      Gizmos.DrawRay(v1 + point, tangent01.normalized * 0.4f);
+      Gizmos.DrawRay(v1 + point, tangent02.normalized * 0.4f);
+      Gizmos.DrawRay(v2 + point, tangent10.normalized * 0.4f);
+      Gizmos.DrawRay(v2 + point, tangent12.normalized * 0.4f);
+      Gizmos.DrawRay(v3 + point, tangent21.normalized * 0.4f);
+      Gizmos.DrawRay(v3 + point, tangent20.normalized * 0.4f);
       Gizmos.color = Color.gray;
       Gizmos.DrawWireSphere(point, 1f);
     }
 
     return interiorAngle;
   }
-  public static float SolidAngleOfTriangle(this Vector3 point, Vector3[] vertices, bool drawGizmos = false) {
-    return (point.SphericalTriangleInteriorAngleSum(vertices, drawGizmos) - 180f) * Mathf.Deg2Rad;
+  public static float SolidAngleOfTriangle(this Vector3 point, Vector3 v1, Vector3 v2, Vector3 v3, bool drawGizmos = false) {
+    return (point.SphericalTriangleInteriorAngleSum(v1, v2, v3, drawGizmos) - 180f) * Mathf.Deg2Rad;
   }
 }
