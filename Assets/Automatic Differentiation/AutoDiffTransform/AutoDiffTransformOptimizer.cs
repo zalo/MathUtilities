6d4e2e2265554051ebcccb5cf37f9e279f6c5e98
Johnathon Selstad <makeshifted@gmail.com>
2018-06-15 11:31:59 -0700

    Add Optimizer Convenience Class

---
+++ Assets/Automatic Differentiation/AutoDiffTransform/AutoDiffTransformOptimizer.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using AutoDiff;
+
+public class AutoDiffTransformOptimizer : MonoBehaviour {
+  public Transform baseTransform;
+  public Mesh lowPolyIcoSphere;
+  public bool fitPosition = true;
+  public bool fitRotation = true;
+  public bool fitScale = false;
+  [Range(0f, 1f)]
+  public float alpha = 0.25f;
+  public bool drawDebug = true;
+
+  DualMatrix4x3 baseTransformMatrix;
+  Optimizer optimizer = new Optimizer();
+
+  Vector3[] icoVerts;
+  void Start() {
+    icoVerts = lowPolyIcoSphere.vertices;
+  }
+
+  void Update() {
+    baseTransformMatrix = new DualMatrix4x3(baseTransform.position, baseTransform.rotation, baseTransform.localScale);
+
+    //Set up the variables to optimize
+    optimizer.Add(transform.position, "position");
+    optimizer.Add(transform.up, "yBasis");
+    optimizer.Add(transform.forward, "zBasis");
+    optimizer.Add(transform.localScale, "scale");
+    optimizer.CalculateErrorDerivative = CalculateErrorDerivative;
+
+    //Calculate and Apply the Instantaneous Gradients at this point in time
+    transform.position -= optimizer.CalcErrorGradient("position") * alpha;
+
+    Vector3 forwardBasis = transform.forward - (optimizer.CalcErrorGradient("zBasis") * alpha);
+    Vector3 upBasis = transform.up - (optimizer.CalcErrorGradient("yBasis") * alpha);
+    transform.rotation = Quaternion.LookRotation(forwardBasis, upBasis);
+
+    transform.localScale -= optimizer.CalcErrorGradient("scale") * alpha;
+  }
+
+  /// <summary>
+  /// Calculates value and derivative of the position of the sphere with respect to whichDeriv
+  /// </summary>
+  float CalculateErrorDerivative() {
+    DualVector3 thisYBasis = optimizer.vec("yBasis"), thisZBasis = optimizer.vec("zBasis");
+    thisYBasis = thisYBasis.Normalize(); thisZBasis = thisZBasis.Normalize();
+    DualVector3 thisXBasis = thisYBasis.Cross(thisZBasis);
+    thisYBasis = thisZBasis.Cross(thisXBasis);
+    
+    //THIS IS A FULLY DIFFERENTIABLE RIGID TRANSFORM - NO SINGULARITIES
+    //Also Single Covers the space (when non-uniform scaling is disabled)
+    DualVector3 scale = optimizer.vec("scale");
+    DualMatrix4x3 thisTransform = new DualMatrix4x3(thisXBasis * scale.x, thisYBasis * scale.y, thisZBasis * scale.z, optimizer.vec("position"));
+    
+    //Sum the Squared Errors
+    DualNumber error = new DualNumber();
+    for (int i = 0; i < icoVerts.Length; i++) {
+      DualVector3 thisPoint = thisTransform * icoVerts[i]; DualVector3 basePoint = baseTransformMatrix * icoVerts[i];
+      if (drawDebug) { Debug.DrawLine(thisPoint, basePoint); }
+      error += (thisPoint - basePoint).SqrMagnitude();
+    }
+    //Divide by Number of Squared Errors
+    error /= icoVerts.Length;
+
+    return error.Derivative;
+  }
+}
---
+++ Assets/Automatic Differentiation/Optimizer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using AutoDiff;
+
+public class Optimizer {
+  public Optimizer() { }
+
+  protected Dictionary<string, DualVector3> vectors = new Dictionary<string, DualVector3>();
+
+  public void Add(Vector3 vector, string key) {
+    if (vectors.ContainsKey(key)) {
+      vectors[key] = vector;
+    } else { vectors.Add(key, vector); }
+  }
+
+  public DualVector3 vec(string key) {
+    DualVector3 toReturn;
+    if(vectors.TryGetValue(key, out toReturn)) {
+      return toReturn;
+    }
+    return toReturn;
+  }
+
+  public Vector3 CalcErrorGradient(string vectorKey) {
+    DualVector3 vector = new DualVector3();
+    if (CalculateErrorDerivative != null && vectors.TryGetValue(vectorKey, out vector)) {
+      Vector3 errorGradient = Vector3.zero;
+      for(int i = 0; i < 3; i++) {
+        vectors[vectorKey] = new DualVector3(vector.Value, i);
+        errorGradient[i] = CalculateErrorDerivative();
+      }
+      vectors[vectorKey] = new DualVector3(vector.Value);
+      return errorGradient;
+    }
+    return Vector3.zero;
+  }
+
+  public System.Func<float> CalculateErrorDerivative;
+
+}
