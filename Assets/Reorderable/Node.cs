6f7e093b648ea7308c095f2845763280c7cff2b7
Johnathon Selstad <jselstad@leapmotion.com>
2019-01-05 12:10:16 -0800

    Add Quick Reorderable List UI Sketch
    https://i.imgur.com/FlcHDJv.gif

---
+++ Assets/Reorderable/Node.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour {
+  public int nodeIndex;
+  public ReorderableList tree;
+  public TextMesh text;
+
+  public bool dragging = false;
+  Vector3 offset = Vector3.zero;
+
+  void Start() {}
+
+  void OnMouseDown() {
+    offset = transform.position - Camera.main.ScreenPointToRay(Input.mousePosition).origin;
+  }
+
+  void OnMouseDrag() {
+    Vector3 curOffset = transform.position - Camera.main.ScreenPointToRay(Input.mousePosition).origin;
+    if (!dragging && offset != curOffset) dragging = true;
+    if (dragging) {
+      transform.position = Camera.main.ScreenPointToRay(Input.mousePosition).origin + offset;
+      tree.reevaluateNodeOrder(this);
+
+      //Transform socket = tree.sockets[tree.nodes.IndexOf(this)];
+      //if (socket != null) {
+      //  transform.position += (socket.position - transform.position) * 0.7f;
+      //}
+    }
+  }
+
+  void OnMouseUp() {
+    if (dragging) {
+      dragging = false;
+    } else {
+      //Activate button
+    }
+  }
+
+  // Update is called once per frame
+  void Update() {
+    Transform socket = tree.sockets[tree.nodes.IndexOf(this)];
+    if (socket != null) {
+      if (!dragging) {
+        transform.position += (socket.position - transform.position) * 0.1f;
+      }
+      transform.rotation = Quaternion.Slerp(transform.rotation, socket.rotation, 0.1f);
+    }
+  }
+}
---
+++ Assets/Reorderable/ReorderableList.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReorderableList : MonoBehaviour {
+  public Transform[] sockets;
+  public GameObject nodePrefab;
+
+  public List<Node> nodes = new List<Node>();
+
+  void Start() {
+    foreach(Transform socket in sockets) {
+      createNode(socket);
+    }
+  }
+
+  public void reevaluateNodeOrder(Node movingNode) {
+    int closestSocket = -1;
+    float minDist = 10000f;
+    for(int i = 0; i < sockets.Length; i++) {
+      float curDist = Vector3.Distance(movingNode.transform.position, sockets[i].position);
+      if (curDist < minDist) {
+        minDist = curDist;
+        closestSocket = i;
+      }
+    }
+
+    if(closestSocket != nodes.IndexOf(movingNode)) {
+      nodes.Remove(movingNode);
+      nodes.Insert(closestSocket, movingNode);
+    }
+  }
+
+  Node createNode(Transform socket) {
+    GameObject prefab = Instantiate(nodePrefab, socket.position, socket.rotation);
+    Node node = prefab.GetComponent<Node>();
+    node.tree = this;
+    node.text.text = "Node: "+socket.name;
+    nodes.Add(node);
+    return node;
+  }
+}
